30ff5d1254effa722189a7e0451322391f356049
Vanilson Sousa <vanilsonped7@gmail.com>
2024-09-28 17:44:36 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio3;
+
+public class Program
+{
+    public static void Main()
+    {
+        string palavra = Console.ReadLine();
+        int totalCaracteres = 0;
+
+        foreach(char letra in palavra)
+        {
+            bool isNumber = char.IsDigit(letra);
+            bool isSymbol = char.IsPunctuation(letra);
+
+            if (letra == ' ' || isNumber) continue;
+            if (isSymbol) continue;
+
+            totalCaracteres++;
+        }
+
+        Console.WriteLine(totalCaracteres);
+    }
+}
